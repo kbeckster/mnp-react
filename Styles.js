1ff09ee2b5bdbce7d9986d0d079a1e5726168219
kbeckster <kohl.rebecca@gmail.com>
2017-05-08 20:16:27 -0500

    latest code from BK

--- Styles.js
+++ Styles.js
@@ -3,48 +3,59 @@ var React = require('react-native');
 
 var StyleSheet = React.StyleSheet.create({
   test: {
-          flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     color: '#FF69B4'
   },
-  container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    tabContainer: {
-      flexDirection: 'row',
-      height: 48,
-    },
-    tab: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin: 4,
-      borderWidth: 1,
-      borderColor: '#ddd',
-      borderRadius: 4,
-    },
-    mapcontainer: {
-      flex: 1,
-       justifyContent: 'center',
-       alignItems: 'center',
-       backgroundColor: '#F5FCFF',
-    },
-    map: {
+  icon: {
+    width: 26,
+    height: 26,
+  },
+  footer:{
+
+  },
+  map: {
       position: 'absolute',
       top: 0,
       right: 0,
       bottom: 0,
       left: 0,
-    },
-    marker: {
-      marginLeft: 46,
-      marginTop: 33,
-      fontWeight: 'bold',
-    }
+  },
+  // container: {
+  //     flex: 1,
+  //     backgroundColor: '#fff',
+  //     alignItems: 'center',
+  //     justifyContent: 'center',
+  //   },
+  //   tabContainer: {
+  //     flexDirection: 'row',
+  //     height: 48,
+  //   },
+  //   tab: {
+  //     flex: 1,
+  //     alignItems: 'center',
+  //     justifyContent: 'center',
+  //     margin: 4,
+  //     borderWidth: 1,
+  //     borderColor: '#ddd',
+  //     borderRadius: 4,
+  //   },
+  //   mapcontainer: {
+  //     flex: 1,
+  //      justifyContent: 'center',
+  //      alignItems: 'center',
+  //      backgroundColor: '#F5FCFF',
+  //   },
+  //   map: {
+  //     position: 'absolute',
+  //     top: 0,
+  //     right: 0,
+  //     bottom: 0,
+  //     left: 0,
+  //   },
+  //   marker: {
+  //     marginLeft: 46,
+  //     marginTop: 33,
+  //     fontWeight: 'bold',
+  //   }
 
 });
 
